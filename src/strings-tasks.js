79bc3f6c7c72882f4678b47ad5ef8766fb1cdd8d
Konstantin Kienya <kienyamail@gmail.com>
2024-11-07 23:11:13 +0300

    feat: add extractNameFromTemplate task solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -401,8 +401,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.split(', ')[1].slice(0, -1);
 }
 
 /**

d177b5c22407f3afde61f8f51740ef7aef640a82
Konstantin Kienya <kienyamail@gmail.com>
2024-11-15 22:32:25 +0300

    feat: add countVowels task solution

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -309,8 +309,11 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  return str
+    .split('')
+    .reduce((acc, letter) => acc + vowels.includes(letter), 0);
 }
 
 /**
